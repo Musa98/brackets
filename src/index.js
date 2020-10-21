2d7024d66d84c92471ac48af291dbbc33ecd620e
Musa98 <www.musik2014@mail.ru>
2020-10-21 12:32:51 +0300

    init: create brackits function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i=0;i<str.length;i++){
+        for(j=0;j<bracketsConfig.length;j++){
+            if(str.includes(bracketsConfig[j][0]+""+bracketsConfig[j][1])){
+            str=str.replace(bracketsConfig[j][0]+""+bracketsConfig[j][1],"");
+            return check(str,bracketsConfig);
+            }
+        }
+    }
+    return str.length==0?true:false;
 }
+    
